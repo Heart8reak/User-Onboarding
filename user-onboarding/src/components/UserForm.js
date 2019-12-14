30680b37c98f68ec4d2fb3520f075ea245740db2
Carlos Baez <carlosbaez@Green-Planet.local>
2019-12-14 00:36:16 -0500

    Added validation to form

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
+import * as Yup from 'yup';
+import axios from "axios";
 
 
 
@@ -19,14 +21,39 @@ const UserForm = ({ values, errors, touched, status }) => {
     return (
         <div className="user-form">
             <Form>
-                <Field type="text" name="name" placeholder="Name" />
-                <Field type="email" name="email" placeholder="Email" />
-                <Field type="password" name="password" placeholder="Password" />
+                <label htmlFor="name">
+                    Name
+                    <Field type="text" name="name" />
+                    {/* <ErrorMessage
+                        name="name"
+                        render={msg => <div className="error">{msg}</div>}
+                    /> */}
+                    {touched.name && errors.name && (
+                        <p className="errors">{errors.name}</p>
+                    )}
+                </label>
+                Email
+                <label htmlFor="email">
+                    <Field type="email" name="email" />
+                    {touched.email && errors.email && (
+                        <p className="errors">{errors.email}</p>
+                    )}
+                </label>
+
+                <label htmlFor="password">
+                    Password
+                    <Field type="password" name="password" />
+                    {touched.password && errors.password && (
+                        <p className="errors">{errors.password}</p>
+                    )}
+                </label>
+
                 <label className="checkbox-container">
                     Terms of Service
-                    <Field type="checkbox" name="termsofservice" checked="{values.termsofservice}" />
+                    <Field type="checkbox" name="termsofservice" checked={values.termsofservice} />
                     <span className="checkmark" />
                 </label>
+
                 <button type="submit">Submit</button>
             </Form>
 
@@ -35,7 +62,6 @@ const UserForm = ({ values, errors, touched, status }) => {
                     <ul key={user.id}>
                         <li>User: {user.name}</li>
                         <li>Email: {user.email}</li>
-                        <li>Terms of Service: {user.termsofservice}</li>
                     </ul>
                 );
             })}
@@ -45,12 +71,34 @@ const UserForm = ({ values, errors, touched, status }) => {
 
 
 const FormUserForm = withFormik({
-    mapPropsToValues({ name, email, termsofservice }) {
+    mapPropsToValues(props) {
         return {
-            name: name || "",
-            email: email || "",
-            termsofservice: termsofservice || false
+            name: props.name || "",
+            email: props.email || "",
+            password: props.password || "",
+            termsofservice: props.termsofservice || false,
         };
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("Please enter a name"),
+        email: Yup.string().required("Please enter your email"),
+        password: Yup.string().required("Please enter your password"),
+        // termsofservice: Yup.boolean()
+    }),
+
+    handleSubmit(values, { setStatus, resetForm }) {
+        console.log("submitting", values);
+        axios
+            .post("https://reqres.in/api/users/", values)
+            .then(res => {
+                console.log("success", res);
+
+                setStatus(res.data);
+
+                resetForm();
+            })
+            .catch(err => console.log(err.response))
     }
 })(UserForm)
 
